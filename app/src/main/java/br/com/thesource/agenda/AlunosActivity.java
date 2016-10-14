5ae671f4d59bcc406150b0d33ff322e224037be4
Felipe de Souza Paiva <spaiva.felipe@gmail.com>
2016-10-14 19:18:59 -0300

    Initial commit

---
+++ app/src/main/java/br/com/thesource/agenda/AlunosActivity.java
@@ -0,0 +1,38 @@
+package br.com.thesource.agenda;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class AlunosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alunos);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        String[] alunos = {"Felipe","Jorge","Mauricio","Jorge","Mauricio","Jorge","Mauricio","Jorge","Mauricio","Jorge","Mauricio","Jorge","Mauricio","Jorge","Mauricio"};
+
+        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goForm = new Intent(AlunosActivity.this,FormularioActivity.class);
+                startActivity(goForm);
+            }
+        });
+    }
+
+}
